cca485c7e774813d43af3257cdfc8d6f00cfa0c4
truthrylynn <runcustic@gmail.com>
2025-08-25 22:22:31 +0900

    [#12] 랜딩 페이지 제작

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,62 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function LandingPage() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    /** Main Container */
+    <div className="flex flex-col">
+      {/** Header */}
+      <div className="flex flex-row justify-between items-center p-[33px]">
+        {/** Logo */}
+        <Image src="/logo/header_logo.svg" alt="Logo" width={71} height={24}/>
+        {/* Authentic Menu */}
+        <div className="flex items-center gap-x-2">
+          {/* 로그인 버튼 */}
+          <button className="px-6 py-2 rounded-full border border-teal-400 text-teal-400 font-bold">
+            로그인
+          </button>
+          {/* 회원가입 버튼 */}
+          <button className="px-6 py-2 rounded-full bg-teal-400 text-white font-bold">
+            회원가입
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      {/** Content */}
+      <div className="flex flex-col justify-center items-center gap-y-[26px] mt-[161px]">
+        {/** Title */}
+        <span className="text-[40px] font-bold">
+          프로젝트 관리의 모든 것, 픽클AI로 간편하게
+        </span>
+        {/** Subtitle */}
+        <span className="text-center whitespace-normal break-words w-[660px] mx-auto text-[16px]">
+          A smart AI-powered platform that lets anyone manage projects like a pro. Intuitive WBS and Gantt UI with automatic predictions enable efficient collaboration and clear project visibility without complexity. 
+        </span>
+        {/** Start Button */}
+        <button className="bg-black text-green-400 font-bold px-5 py-2 rounded-full hover:bg-gray-900 transition">
+          Pickle 무료로 시작하기
+        </button>
+        {/** Logo Image */}
+        <div className="w-full h-[271px] bg-[url('/landing/logo_band.svg')] bg-repeat-x bg-center bg-[length:820px_271px] mt-[91px]"> 
+        </div>
+      </div>
+      {/** Footer */}
+      <div className="flex flex-row justify-between px-[48px] py-[38px] bg-[#212121]/5 mt-[20px]">
+        {/** Logo & Copyright */}
+        <div className="flex flex-col gap-y-[96px]">
+          <Image src="/logo/footer_logo.svg" alt="Logo" width={48} height={38}/>
+          <div className="text-[12px] text-[#828282]">
+            @ 2025 Pickle AI. All rights reserved.
+          </div>
+        </div>
+        {/** Contact */}
+        <div className="flex flex-col gap-y-[10px]">
+          <span className="text-lg">
+            Let's Talk
+          </span>
+          <span className="text-[12px] text-[#212121] underline opacity-60">
+            contact@yourdomain.com
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
